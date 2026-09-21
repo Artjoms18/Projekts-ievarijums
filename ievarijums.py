62a76f0960c9f194ae73d7d0f2c184a4eed1370d
Artjoms18 <artjompliska7@gmail.com>
2026-09-21 09:37:20 +0300

    Darbs

--- ievarijums.py
+++ ievarijums.py
@@ -1,3 +1,11 @@
+receptes_numurs=int(input("Ievadiets receptes numuru:\n1) 1 kg ābolu = 300 gr. cukura\n2) 1 kg ābolu = 500 gr.cukura\n"))
+
 cukura_cena= float(input("Ievadi cukura cenu:"))
 aboli_kg= float(input("Ievadi, cik ābolu tev ir(kg): "))
+if receptes_numurs==1:
+    izmaksa= cukurs_uz_kg*0.3
+else:
+    izmaksa= cukurs_uz_kg*0.5
+
+
 print(f"Par cukuru tu samaksāsi {cukura_cena*aboli_kg*0.7} eiro ")
